cf07b0cea3943ea2db6ccd2e1edff6fe16baf32e
MVDW <maxvdwolf@gmail.com>
2023-09-18 14:25:44 +0200

    Add grid layout

--- src/ui/layouts/Grid.js
+++ src/ui/layouts/Grid.js
@@ -1,5 +1,16 @@
-const Grid = () => {
+const Grid = (items) => {
+    const el_grid = document.createElement("div");
+    el_grid.classList.add("grid");
+
+    for (let i = 0; i < items.length; i++) {
+        const el_grid_item = document.createElement("div");
+        el_grid_item.classList.add("item");
+        
+        el_grid_item.appendChild(items[i]);
+        el_grid.appendChild(el_grid_item);
+    }
     
+    return el_grid;
 
 }
 
